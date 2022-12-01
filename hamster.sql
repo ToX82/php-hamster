aed4558269cd9ef1e070821d0acd844bb2fe0e3e
simoneturco03 <117657105+simoneturco03@users.noreply.github.com>
2022-12-01 16:29:38 +0100

    Add description field to activities

--- hamster.sql
+++ hamster.sql
@@ -1,62 +1,100 @@
--- Adminer 4.7.8 MySQL dump
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Nov 25, 2022 alle 21:09
+-- Versione del server: 10.4.18-MariaDB
+-- Versione PHP: 8.0.3
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `activities`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hamster`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `activities`
+--
+
 CREATE TABLE `activities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `activity` varchar(255) NOT NULL,
   `tag` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `start` datetime NOT NULL,
   `end` datetime DEFAULT NULL,
-  `duration_minutes` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `duration_minutes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `autologins`;
+--
+-- Struttura della tabella `autologins`
+--
+
 CREATE TABLE `autologins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
   `created` datetime NOT NULL,
   `expires` datetime NOT NULL,
   `browser` varchar(255) NOT NULL,
-  `ip` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
+  `ip` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `roles`;
+--
+-- Struttura della tabella `roles`
+--
+
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `position` int(11) NOT NULL DEFAULT 99,
-  `hidden` tinyint(4) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `hidden` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `roles`
+--
 
 INSERT INTO `roles` (`id`, `name`, `description`, `position`, `hidden`) VALUES
-(1,	'admin',	'Amministratore',	1,	0),
-(2,	'user',	'Utente',	2,	0);
+(1, 'admin', 'Amministratore', 1, 0),
+(2, 'user', 'Utente', 2, 0);
 
-DROP TABLE IF EXISTS `roles_users`;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `roles_users`
+--
+
 CREATE TABLE `roles_users` (
   `role_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  KEY `index_roles_users_on_role_id` (`role_id`),
-  KEY `index_roles_users_on_user_id` (`user_id`)
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
+--
+-- Struttura della tabella `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -65,9 +103,73 @@ CREATE TABLE `users` (
   `active` tinyint(4) NOT NULL DEFAULT 1,
   `last_login_at` datetime DEFAULT NULL,
   `last_login_ip` varchar(50) DEFAULT NULL,
-  `login_count` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `login_count` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indici per le tabelle scaricate
+--
 
--- 2020-12-25 21:38:06
+--
+-- Indici per le tabelle `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `autologins`
+--
+ALTER TABLE `autologins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `roles_users`
+--
+ALTER TABLE `roles_users`
+  ADD KEY `index_roles_users_on_role_id` (`role_id`),
+  ADD KEY `index_roles_users_on_user_id` (`user_id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT per la tabella `autologins`
+--
+ALTER TABLE `autologins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT per la tabella `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
